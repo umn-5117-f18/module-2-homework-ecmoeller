82d8c72ad75119e4e13ffe16177512638b4064f1
Emily <ecmoeller@wisc.edu>
2018-11-11 19:21:27 -0600

    Set up initial app

---
+++ todo/src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAdynoreLJg8vdGFklI5EVuo5eMr7R699Y",
+    authDomain: "todo-aa404.firebaseapp.com",
+    databaseURL: "https://todo-aa404.firebaseio.com",
+    projectId: "todo-aa404",
+    storageBucket: "todo-aa404.appspot.com",
+    messagingSenderId: "148072616028"
+  };
+  firebase.initializeApp(config);
---
+++ todo/src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import VueFire from "vuefire";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+import App from './App.vue';
+import router from './router';
+import "./firebaseConfig";
+
+
+Vue.use(VueFire);
+
+Vue.config.productionTip = false;
+
+export const db = firebase.firestore();
+console.log("Here is db" + db);
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
+
